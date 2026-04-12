83a4a0a58dcd068a0a24ebf472ce2766e0958a2e
Andrew Atonya <atonyaandrew1@gmail.com>
2026-04-12 13:06:45 +0300

    saving to a file trial

--- trial.py
+++ trial.py
@@ -25,6 +25,49 @@ class BankAccount:
         print(f"Total Balance: {self.__balance}")
         print("_"*20)
 
-acc = BankAccount(input("enter your name: "),input("account no: "),int(input("enter initial balance: ")))
-acc.deposit(int(input("How much do you want to deposit? ")))
-acc.print_receipt()
+#acc = BankAccount(input("enter your name: "),input("account no: "),int(input("enter initial balance: ")))
+#acc.deposit(int(input("How much do you want to deposit? ")))
+#acc.print_receipt()
+
+
+class Student:
+    def __init__(self,name,regNumber,year):
+        self.name =name
+        self.regNumber = regNumber
+        self.year = year
+        self.marks =[]
+    def input_marks(self):
+        print(f"Inputing marks for{self.name}:")
+        for i in range(1,6):
+            score= float(input(f"unit{i}mark:"))
+            self.marks.append(score)
+   
+    def get_average(self):
+        if len(self.marks) ==0:return 0
+        return sum(self.marks)/len(self.marks)
+
+    def get_grade(self):
+        average = self.get_average()
+        if not self.marks:
+            return "No marks entered"
+        if average >=70:return "A"
+        if average >=60: return "B"
+        if average >= 50: return "C"
+        if average >=40: return "D"
+        else: return "E"
+    def save_data(self):
+        with open("student record.txt","a")as file:
+            file.write(f"name:{self.name}|marks:{self.input_marks()}|Average:{self.get_average():2f}|Grade:{self.get_grade()}\n")
+            print("Successfully savedto student_record.txt")
+
+
+student= Student(input("enter student name: "),input("registration number: "),input("year of study: ")) 
+student.input_marks()
+print(f"Student: {student.name}")
+print(f"Average Score: {student.get_average():2f}")
+print(f"Final grade: {student.get_grade()}")
+student.save_data()
+
+
+
+   
